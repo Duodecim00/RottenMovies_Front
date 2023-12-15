42f0cb7c4e2c8d73f1e71ea5fd0bc5becf1730a3
Cesar P <capr99@hotmail.com>
2023-12-15 03:01:23 -0400

    vistas de chat

--- src/navigators/appNavigation.js
+++ src/navigators/appNavigation.js
@@ -11,12 +11,16 @@ const {primary, tertiary} = Colors;
 import Login from "../screens/login/Login";
 import Register from "../screens/register/Register";
 import Profile from "../screens/profile/Profile";
-
+import GlobalState from "../context";
+import Messagescreen from '../screens/messageScreen/MessageScreen';
+import Chatscreen from '../screens/chatScreen/ChatScreen';
 const Stack = createNativeStackNavigator();
 
 
 export default function AppNavigation() {
   return (
+    <GlobalState>
+
     <NavigationContainer>
       <Stack.Navigator  screenOptions={{
                     headerStyled:{
@@ -30,15 +34,22 @@ export default function AppNavigation() {
                     } 
                 }}
                 initialRouteName="Home">
-        <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
-        <Stack.Screen name="Movie" options={{headerShown: false}} component={MovieScreen} />
-        <Stack.Screen name="Person" options={{headerShown: false}} component={PersonScreen} />
-        <Stack.Screen name="Search" options={{headerShown: false}} component={SearchScreen} />
+                    <Stack.Screen
+            name="Chatscreen"
+            component={Chatscreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Messagescreen" component={Messagescreen} />
+        {/* <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} /> */}
+        {/* <Stack.Screen name="Movie" options={{headerShown: false}} component={MovieScreen} /> */}
+        {/* <Stack.Screen name="Person" options={{headerShown: false}} component={PersonScreen} /> */}
+        {/* <Stack.Screen name="Search" options={{headerShown: false}} component={SearchScreen} /> */}
         {/* <Stack.Screen name="Register" options={{headerShown: false}} component={Register}/> */}
         {/* <Stack.Screen nrame="Login" options={{headerShown: false}} component={Login}/> */}
         {/* <Stack.Screen options={{headerTintColor: primary}} name="Profile" component={Profile}/> */}
       </Stack.Navigator>
     </NavigationContainer>
+    </GlobalState>
   );
   
 }
